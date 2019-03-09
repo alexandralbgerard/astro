569efc6bd02a9b17d46ab1803f13807dc808a267
Alexandra Gerard <alexandralbgerard@gmail.com>
2019-03-09 12:34:31 -0600

    changed variable option back to original config

--- client/src/components/AstroDetail.js
+++ client/src/components/AstroDetail.js
@@ -1,44 +1,36 @@
 import React, { Component } from 'react';
-import { graphql, Query } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import { getAstroSignQuery } from '../queries/queries';
 
-export default class AstroDetail extends Component {
+class AstroDetail extends Component {
   displayAstroDetails() {
-    console.log('THIS.PROPS.SIGN', this.props.sign);
-    return (
-      <Query query={getAstroSignQuery} variables={{ sign: this.props.sign }}>
-        {({ loading, error, data }) => {
-          if (loading) return null;
-          if (error) return `Error!: ${error}`;
-
-          console.log('DATA', data);
-          const { AstrologySign } = data;
-
-          return (
-            <div>
-              <p>{AstrologySign.dates}</p>
-              <p>{AstrologySign.element}</p>
-              <p>{AstrologySign.ruler}</p>
-              <p>{AstrologySign.stregnths}</p>
-              <p>{AstrologySign.weaknesses}</p>
-              <p>{AstrologySign.overview}</p>
-            </div>
-          );
-        }}
-      </Query>
-    );
+    const { AstrologySign } = this.props.data;
+    if (AstrologySign) {
+      return (
+        <div>
+          <p>{AstrologySign.dates}</p>
+          <p>{AstrologySign.element}</p>
+          <p>{AstrologySign.ruler}</p>
+          <p>{AstrologySign.stregnths}</p>
+          <p>{AstrologySign.weaknesses}</p>
+          <p>{AstrologySign.overview}</p>
+        </div>
+      );
+    } else {
+      return <div>No book selected...</div>;
+    }
   }
   render() {
     return <div id="astro-details">{this.displayAstroDetails()}</div>;
   }
 }
 
-// export default graphql(getAstroSignQuery, {
-//   options: props => {
-//     return {
-//       variables: {
-//         sign: props.sign,
-//       },
-//     };
-//   },
-// })(AstroDetail);
+export default graphql(getAstroSignQuery, {
+  options: props => {
+    return {
+      variables: {
+        sign: props.sign,
+      },
+    };
+  },
+})(AstroDetail);
